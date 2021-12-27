4649bdbfc4ac92556114b10865fdb434273f4ae0
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-27 18:57:16 -0500

    create giftExpertApp component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,9 @@
+export const GifExpertApp = () => {
+    const categories = ['One punch man','Naruto','Dragon Ball']
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <hr/>
+        </div>
+    )
+}
