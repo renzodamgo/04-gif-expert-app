a9698978249a8085fc6f61c202f25c043dd2b7ae
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-28 00:12:52 -0500

    Add delay to fade in

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ({ id, title, url }) => {
     // console.log({id, title, url})
 
     return (
-        <div className='card animate__animated animate__fadeIn'>
+        <div className='card animate__animated animate__fadeIn animate__delay-0.5s'>
             <img src={url} alt={title} />
             <p>
                 {title}
