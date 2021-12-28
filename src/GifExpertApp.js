290f364b4b662f70fcfaca8c3cf8e05eb4de25be
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-27 19:21:08 -0500

    Add Digimon to list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,27 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
-    const categories = ['One punch man','Naruto','Dragon Ball']
+    // const categories = ['One punch man','Naruto','Dragon Ball']
+    const [categories, setCategories] = useState(['One punch man', 'Naruto', 'Dragon Ball'])
+
+    const handleAdd = () => {
+        // setCategories([...categories, 'Digimon'])
+        // Set categories regresa un callback con el elemento usado
+        setCategories(cats =>[...cats, 'Digimon'])
+    }
+
     return (
         <div>
             <h2>GifExpertApp</h2>
-            <hr/>
+            <hr />
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}> {category} </li>
+                    })
+                }
+            </ol>
         </div>
     )
 }
