ff331fe6684908219c4ea36ef9686bbb596ecb32
Renzo Damian <renzo.damian.go@gmail.com>
2021-12-27 23:04:45 -0500

    use encoding uri to encode category API request

--- src/components/GiftGrid.js
+++ src/components/GiftGrid.js
@@ -9,8 +9,7 @@ export const GiftGrid = ({ category }) => {
     }, []) // solo se ejecuta una vez
 
     const getGifts = async () => {
-        const trimCategory = category.replace(/\s/g, '+')
-        const url = `https://api.giphy.com/v1/gifs/search?q=${trimCategory}&limit=5&api_key=cHrzYubYZ4nTPGmAmQrGm1aDRtL1Vlr5`
+        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=cHrzYubYZ4nTPGmAmQrGm1aDRtL1Vlr5`
         console.log(url)
         const resp = await fetch(url);
         const { data } = await resp.json();
